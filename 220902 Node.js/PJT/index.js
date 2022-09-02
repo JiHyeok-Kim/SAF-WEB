8598e101b887fcfb646074d2255cc33fcdeaf988
JiHyeok-Kim <hoggyy@naver.com>
2022-09-02 14:59:22 +0900

    220902

---
+++ 220902 Node.js/PJT/index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+
+const app = express();
+const PORT = 8080;
+
+const cors = require("cors");
+app.use(cors());
+
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+const { logger } = require("./utils/winsotn");
+
+app.use(express.static(__dirname + "/views"));
+
+const fs = require("fs");
+
+const insert = (str, index, target) => {
+  const front = str.slice(0, index);
+  const back = str.slice(index, str.length);
+  return front + target + back;
+};
+
+let retData = {};
+
+app.get("/api/logs", async (req, res) =>{
+  res.json(retData);
+})
+
+app.post("/api/logs", async (req, res) => {
+  logger.error("error 메시지");
+  logger.warn("warn 메시지");
+  logger.info("info 메시지");
+  logger.http("http 메시지");
+  logger.debug("debug 메시지");
+  fs.readFile("./logs/2022-09-02-11.log", "utf8", (err, data) => {
+    retData = data;
+
+    let idx = - 1;
+    while(1){
+      idx = retData.indexOf("}", idx+1);
+      if(idx === -1) { break;}
+      retData = insert(retData, idx + 1, ",");
+    }
+    retData = "[" + retData.slice(0, retData.length - 3) + "]";
+    retData = JSON.parse(retData);
+    console.log(retData);
+  });
+  return res.json({
+    test: "OK",
+  });
+});
+
+app.listen(PORT, () => console.log(`this server listening on ${PORT}`));
